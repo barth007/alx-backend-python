8dba9513d0458694a577ce1399532b496b3fe8c7
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-10-06 06:45:14 +0000

    done with task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import math
+
+"""
+The modules takes n as arguments as type as float and
+returns the floor value of n
+"""
+
+
+def floor(n: float) -> int:
+    """
+    evalutes the floor value of n
+    """
+
+    result: float = math.floor(n)
+    return result
