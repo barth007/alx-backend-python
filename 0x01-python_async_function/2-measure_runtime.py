88dfd2bf2b99943ee912c47bd448e9585d7aebab
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-10-09 16:17:56 +0000

    added task 2

---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+A function that takes integers n and max_delay
+evaluates the time taken for a program to execute
+"""
+
+
+import time
+import asyncio
+wait_n = __import__("1-concurrent_coroutines").wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    evaluates the time taken for a program to execute
+    """
+
+    start_time: float = time.perf_counter()
+    asyncio.run(wait_n(n, max_delay))
+    end_time: float = time.perf_counter()
+    total_time: float = (end_time - start_time) / n
+    return total_time
