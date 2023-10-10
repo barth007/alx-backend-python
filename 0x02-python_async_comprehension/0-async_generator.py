7276e106482eaff1531a7401e2b1520223b9d0a0
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-10-10 12:24:41 +0000

    updated 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,11 +4,11 @@ A coroutine that loops 10 times and asychronically await
 and then yield random number from 1 - 10
 """
 import asyncio
-from typing import AsyncGenerator
+from typing import Iterator
 import random
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Iterator[float]:
     """
     yield a random number while looping
     """
