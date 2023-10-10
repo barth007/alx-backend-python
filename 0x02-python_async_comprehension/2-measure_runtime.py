d376ab57c9c661fe496409157a39945bfa452169
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-10-10 12:09:49 +0000

    added task2

---
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""
+A coroutine that execute async_commprehension 4 times
+using async.gather() and measures the execution time
+returns time_taken
+"""
+import asyncio
+import time
+async_comprehension = __import__('1-async_comprehension').async_comprehension
+
+
+async def measure_runtime() -> float:
+    """
+    measures the runtime of async_comprehension
+    """
+
+    start_time = time.perf_counter()
+    await asyncio.gather(
+            async_comprehension(),
+            async_comprehension(),
+            async_comprehension(),
+            async_comprehension()
+            )
+    end_time = time.perf_counter()
+    time_taken: float = end_time - start_time
+    return time_taken
