75165018e3cffe30757d8ac44dd0f153e4cc9f45
barth007 <bartholomew.bassey@st.futminna.edu.ng>
2023-10-11 04:11:23 +0000

    updated task1

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -15,12 +15,7 @@ async def measure_runtime() -> float:
     """
 
     start_time = asyncio.get_event_loop().time()
-    await asyncio.gather(
-            async_comprehension(),
-            async_comprehension(),
-            async_comprehension(),
-            async_comprehension()
-            )
+    await asyncio.gather(*(async_comprehension() for _ in range(4)))
     end_time = asyncio.get_event_loop().time()
     time_taken: float = end_time - start_time
     return time_taken
